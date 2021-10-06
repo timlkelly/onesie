a85ee8c6029cdd0caac4aaa8340a0df141eb73cf
Tim Kelly <timlkelly@users.noreply.github.com>
2021-10-06 16:48:53 -0500

    Rename onesie table

--- lib/generators/onesie/templates/migration.rb
+++ lib/generators/onesie/templates/migration.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-class CreateOnesieTable < ActiveRecord::Migration<%= migration_version %>
+class CreateOnesieLogsTable < ActiveRecord::Migration<%= migration_version %>
   def change
-    create_table :onesies do |t|
-      t.string :filename, null: false
-      t.index :filename, unique: true
+    create_table :onesie_logs do |t|
+      t.string :task_name, null: false
+      t.index :task_name, unique: true
 
       t.timestamps null: false
     end
