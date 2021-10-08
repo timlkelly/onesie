958a9bf3f3f646ec59e8f8a899667952134977b0
Tim Kelly <timlkelly@users.noreply.github.com>
2021-10-07 21:13:49 -0500

    Add error messaging for failed tasks

--- lib/onesie/overseer.rb
+++ lib/onesie/overseer.rb
@@ -13,11 +13,22 @@ module Onesie
           klass = Onesie::Tasks.const_get(const)
           klass.class_eval { prepend Onesie::TaskWrapper }
           klass.new.run
+        rescue StandardError => e
+          puts error_message(const).red
+          raise e
         end
       end
     end
 
     private
+
+    def error_message(const)
+      <<~MSG
+        \n
+        An error occured while running #{const} task.
+        All further tasks did not run.
+      MSG
+    end
 
     def load_tasks
       Pathname(Onesie::TASKS_DIR).children.each { |child| load child }
