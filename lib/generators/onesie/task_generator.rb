ccb1d1f5030d709f8f00d0ba4259a7474ad28c08
Tim Kelly <timlkelly@users.noreply.github.com>
2021-10-13 17:25:20 -0500

    Add TaskGenerator spec

--- lib/generators/onesie/task_generator.rb
+++ lib/generators/onesie/task_generator.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+require 'rails/generators/named_base'
+
 module Onesie
   module Generators
     # Generates a new Onesie Task template
     class TaskGenerator < Rails::Generators::NamedBase
+      desc 'Generate a new Onesie Task'
       source_root File.expand_path('./templates', __dir__)
 
       def create_task
---
+++ spec/generators/task_generator_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'generator_spec'
+require 'generators/onesie/task_generator'
+
+RSpec.describe Onesie::Generators::TaskGenerator, type: :generator do
+  destination File.expand_path('../../tmp', __dir__)
+  arguments %w(TestTask)
+
+  before do
+    prepare_destination
+    run_generator
+  end
+
+  it 'creates a new task file' do
+    assert_file 'onesie/tasks/test_task.rb', /class TestTask/
+  end
+end
