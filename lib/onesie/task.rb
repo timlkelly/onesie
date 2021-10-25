1eced80270753ba122efc0a9f2d160b88d97072a
Tim Kelly <timlkelly@users.noreply.github.com>
2021-10-24 21:08:00 -0500

    Add Onesie::Task.manual_task documentation

--- lib/onesie/task.rb
+++ lib/onesie/task.rb
@@ -3,6 +3,13 @@
 module Onesie
   # Base Task class
   class Task
+    # Class macro to define #manual_task? guard method
+    #
+    # @api public
+    # @param enabled [Boolean]
+    # @example Declare if a Task can only be run manually
+    #  manual_task enabled: true
+    # @return [Symbol]
     def self.manual_task(enabled: false)
       define_method(:manual_task?) { enabled }
     end
