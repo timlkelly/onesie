3b1cf902b0c1cde9c0e3b25046b9e75154f332c9
Tim Kelly <timlkelly@users.noreply.github.com>
2021-11-06 14:56:35 -0500

    Redesign Manager and Task loading process

--- spec/onesie/manager_spec.rb
+++ spec/onesie/manager_spec.rb
@@ -2,43 +2,14 @@
 
 RSpec.describe Onesie::Manager, order: :custom do
   let(:manager) { subject }
-  let(:task) { instance_double(Onesie::Tasks::TestTask, run: true) }
 
-  before do
-    stub_const('Onesie::TASKS_DIR', 'spec/support/tasks')
-  end
-
-  describe '#initialize' do
-    it 'loads the available Tasks', order_number: 0 do
-      expect {
-        manager
-      }.to change(Onesie::Tasks, :constants).from([]).to([:TestTask])
-    end
-  end
-
-  describe '#run_task' do
-    it 'prepends the TaskWrapper', order_number: 1 do
-      manager.run_task(:TestTask)
-
-      expect(Onesie::Tasks::TestTask.ancestors).to include(Onesie::TaskWrapper)
+  describe '#tasks' do
+    before do
+      allow(described_class).to receive(:tasks_paths).and_return(['spec/support/tasks'])
     end
 
-    it 'instantiates a new Task instance', order_number: 2 do
-      mngr = manager
-      allow(Onesie::Tasks::TestTask).to receive(:new).and_return(task)
-
-      mngr.run_task(:TestTask)
-
-      expect(Onesie::Tasks::TestTask).to have_received(:new)
-    end
-
-    it 'runs the Task', order_number: 3 do
-      mngr = manager
-      allow(Onesie::Tasks::TestTask).to receive(:new).and_return(task)
-
-      mngr.run_task(:TestTask)
-
-      expect(task).to have_received(:run)
+    it 'returns an array of TaskProxies' do
+      expect(manager.tasks).to include(Onesie::TaskProxy)
     end
   end
 end
