5cdbd4ce5f53fc00a68443b16451b198fca9bd37
Tim Kelly <timlkelly@users.noreply.github.com>
2021-12-13 09:22:19 -0600

    Update Manager#run_task param specificity

--- lib/onesie/task_proxy.rb
+++ lib/onesie/task_proxy.rb
@@ -2,13 +2,17 @@
 
 module Onesie
   # TaskProxy is a stand in for the actual Task until the class is needed
-  TaskProxy = Struct.new(:name, :version, :filename, :priority) do
-    def initialize(name, version, filename, priority)
+  TaskProxy = Struct.new(:name, :version, :file_path, :priority) do
+    def initialize(name, version, file_path, priority)
       super
       @task = nil
     end
 
     delegate :run, to: :task
+
+    def filename
+      file_path.match(/([0-9]+_[_a-z0-9]+)/).captures.first
+    end
 
     private
 
@@ -17,7 +21,7 @@ module Onesie
     end
 
     def load_task
-      require(File.expand_path(filename))
+      require(File.expand_path(file_path))
       klass = "Onesie::Tasks::#{name}".constantize
       klass.class_eval { prepend Onesie::TaskWrapper }
       klass.new(name, version)
